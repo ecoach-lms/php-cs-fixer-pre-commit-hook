79db3a9e34154a19b764c2b75924c2a15d8249f6
Michael Ralston <michael@ralston.id.au>
2015-11-24 09:00:59 +1100

    New pre commit hook of debatable utility

---
+++ pre_commit_hooks/php-cs-fixer-dry-run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/zsh
+
+y=0
+for x in "$@"; do
+  ./bin/php-cs-fixer fix $x --dry-run --config=sf23 --level=symfony -n --ansi --format=txt --diff
+  err=$?
+  [[ $y = 0 ]] && y=$err
+done
+exit $y
