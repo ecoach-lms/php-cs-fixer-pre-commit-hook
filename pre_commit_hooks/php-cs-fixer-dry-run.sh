e3ed948e25f2d19daf5f3b411b68bde1a8962283
David McLean <gopsychonauts@gmail.com>
2015-12-08 09:36:39 +1100

    Use concatenation WITH spaces, because honestly.

--- pre_commit_hooks/php-cs-fixer-dry-run.sh
+++ pre_commit_hooks/php-cs-fixer-dry-run.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env zsh
+fixers=(
+  concat_with_spaces
+  -concat_without_spaces
+)
 
 y=0
 for x in "$@"; do
-  ./bin/php-cs-fixer fix $x --dry-run --config=sf23 --level=symfony -n --ansi --format=txt --diff
+  ./bin/php-cs-fixer fix $x --dry-run --config=sf23 --level=symfony --fixers=${(j:,:)fixers} -n --ansi --format=txt --diff
   err=$?
   [[ $y = 0 ]] && y=$err
 done
