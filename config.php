b7a40a3a1c18f238a5d9f3fefdbbd326f5cf60aa
Danielle McLean <dani@00dani.me>
2018-07-19 11:47:03 +1000

    Alphabetise the rules we use - I thought I'd done this already but I messed up oops

--- config.php
+++ config.php
@@ -5,10 +5,10 @@ $rules = [
 	'@PSR2' => true,
 	'@Symfony' => true,
 	'array_syntax' => ['syntax' => 'short'],
+	'blank_line_before_statement' => false,
 	'multiline_whitespace_before_semicolons' => true,
 	'ordered_imports' => true,
 	'phpdoc_order' => true,
-	'blank_line_before_statement' => false,
 	'phpdoc_to_comment' => false,
 ];
 
