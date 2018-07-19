07f956a74590e4711b89b7e9cdc76f72b2c668af
Danielle McLean <dani@00dani.me>
2018-07-19 14:38:58 +1000

    Adjust concat_space rule to expect spaces rather than a lack of spaces :o

--- config.php
+++ config.php
@@ -6,6 +6,7 @@ $rules = [
 	'@Symfony' => true,
 	'array_syntax' => ['syntax' => 'short'],
 	'blank_line_before_statement' => false,
+	'concat_space' => ['spacing' => 'one'],
 	'multiline_whitespace_before_semicolons' => true,
 	'ordered_imports' => true,
 	'phpdoc_order' => true,
